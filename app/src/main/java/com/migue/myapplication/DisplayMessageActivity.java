704109f0944316b4e75eacc6d704c2701f4db35b
Clyde Migue <clyde.migue@gmail.com>
2018-07-28 17:33:26 +0800

    03 laboratory exercise 1

---
+++ app/src/main/java/com/migue/myapplication/DisplayMessageActivity.java
@@ -0,0 +1,22 @@
+package com.migue.myapplication;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+public class DisplayMessageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_display_message);
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+        TextView textView = (TextView) findViewById(R.id.txt_view);
+        textView.setText(message);
+
+    }
+}
